4c20baa4800839dee2bd4caed8b13d77dfc4790d
Wood, Bryan Michael <woodbrmi@iu.edu>
2023-03-22 17:19:10 -0400

    Initial commit

---
+++ src/main/java/edu/iu2/demo/controller/GreetingController.java
@@ -0,0 +1,22 @@
+package edu.iu2.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//annotate as rest controller
+//we also want requests to this controller
+@RestController
+@RequestMapping
+public class GreetingController {
+
+    //Get https:localhost:8080 <- we retrieve this function upon request
+    //we add mapping to handle any http get request coming to application
+    //we can add arguments to GetMapping("/requestN") for additional requests
+    @GetMapping
+    //add method to return greetings
+    public String greeting()
+    {
+        return "Greeting...";
+    }
+}
