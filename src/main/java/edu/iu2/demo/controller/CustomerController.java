e5527eaa33c4ef4c2644f84ce25320c9b005aa36
bwood3 <bryanwood1207@gmail.com>
2023-04-05 18:15:42 -0400

    docker added

--- src/main/java/edu/iu2/demo/controller/CustomerController.java
+++ src/main/java/edu/iu2/demo/controller/CustomerController.java
@@ -1,12 +1,21 @@
 package edu.iu2.demo.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
 import edu.iu2.demo.model.Customer;
 import edu.iu2.demo.repository.CustomerRepository;
-import edu.iu2.demo.repository.InMemoryCustomerRepository;
 import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-import java.util.List;
 
 //tell framework this is a controller
 @RestController
@@ -33,10 +42,10 @@ public class CustomerController{
         return repository.findAll();
     }
 
-    //find by ID added
-    //map url to ID added to GetMapping argument
-    @GetMapping("/{id}")
-    public Customer findByID(@PathVariable int id){return repository.getCustomerByID(id);}
+//    //find by ID added
+//    //map url to ID added to GetMapping argument
+//    @GetMapping("/{id}")
+//    public Customer findByID(@PathVariable int id){return repository.getCustomerByID(id);}
 
 
     //http post request comes to application
--- src/main/java/edu/iu2/demo/model/Customer.java
+++ src/main/java/edu/iu2/demo/model/Customer.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+//import javax.persistence.Id;
 import jakarta.validation.constraints.NotEmpty;
 import jdk.jfr.Enabled;
 
